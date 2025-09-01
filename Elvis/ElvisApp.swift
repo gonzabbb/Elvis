e4f422f559b8c51268229117ae00bbbdcc9dbb05
Abraham Gonzalez Diaz <gonzabbb@bc.edu>
2025-09-01 16:03:06 -0400

    Initial Commit

---
+++ Elvis/ElvisApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElvisApp.swift
+//  Elvis
+//
+//  Created by Abraham Gonzalez on 9/1/25.
+//
+
+import SwiftUI
+
+@main
+struct ElvisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
