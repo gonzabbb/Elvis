7ada1997b69aa4b40d4905aba589e8e28b020ea1
Abraham Gonzalez Diaz <gonzabbb@bc.edu>
2025-09-01 16:16:22 -0400

    Peace Love and Understanding Challenge

--- Elvis/ContentView.swift
+++ Elvis/ContentView.swift
@@ -8,14 +8,54 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var  message = ""
+    @State private var image = ""
+    
     var body: some View {
+        
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("What's So Funny 'Bout")
+                .font(.largeTitle)
+                .fontWeight(.black)
+                .foregroundStyle(Color.purple)
+            
+            Spacer()
+            
+            Image(systemName: image)
+                .resizable()
+                .scaledToFit()
+                .frame(width: 300, height: 300)
+           
+            Text(message)
+                .font(.largeTitle)
+                .fontWeight(.black)
+                .foregroundStyle(Color.purple)
+            
+            Spacer()
+            
+            HStack{
+                Button("Peace") {
+                    message = "Peace"
+                    image = "peacesign"
+                }
+                
+                
+                Button("Love") {
+                    message = "Love"
+                    image = "heart"
+                }
+                
+                Button("Understanding") {
+                    message = "understanding"
+                    image = "lightbulb"
+                }
+            }
+            .buttonStyle(.borderedProminent)
+            .font(.title2)
+            .tint(.purple)
         }
         .padding()
+        
     }
 }
 
